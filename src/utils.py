2e0c328579b1595975c4832bdcc36723387bb753
abhishek <bhatiaabhishek8893@gmail.com>
2021-05-01 18:11:13 +0530

    Added LabelAnalyzer to src.utils

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ Utility functions
 '''
 
 import numpy as np
+import pandas as pd
 
 def describe_tensor(tensor):
     '''Describe a tensor (print out useful info)'''
@@ -26,3 +27,47 @@ def get_dataframe_cols(df, cols):
 
 def rmse(y_true, y_pred):
     return np.sqrt(np.mean((y_true - y_pred)**2))
+
+
+
+class LabelAnalyzer:
+    
+    def __init__(self, train_labels, test_labels):
+        self.train = self._get_count_dict(train_labels)
+        self.test = self._get_count_dict(test_labels)
+        
+        self._make_count_df()
+
+    
+    @staticmethod
+    def _get_count_dict(labels):
+        unique, counts = np.unique(labels, return_counts=True)
+        return dict(zip(['label', 'count'], [list(unique), list(counts)]))
+        
+    def count(self, label, subset='train'):
+        d = self.__getattribute__(subset)
+        idx = d['label'].index(label)
+        return d['count'][idx]
+    
+    def _make_count_df(self):
+        traindf = pd.DataFrame(self.train)
+        testdf = pd.DataFrame(self.test)
+        countdf = pd.merge(traindf, testdf, how='outer', on='label', 
+                           suffixes=('_train', '_test')).fillna(0).sort_values('label')
+        
+        countdf[['count_train', 'count_test']] = countdf[['count_train', 'count_test']].astype('Int64')
+        
+        self.countdf = countdf
+        
+        
+    def plot(self):
+        ax = self.countdf.plot(x='label', kind='bar', stacked=True, figsize=(12, 4))
+        ax.legend(labels=['train', 'test'], bbox_to_anchor=(1.01, 0.6))
+        ax.set_title('Train vs Test label distribution', fontdict=dict(weight='bold', size=15))
+        return ax
+    
+            
+        
+    
+            
+        
