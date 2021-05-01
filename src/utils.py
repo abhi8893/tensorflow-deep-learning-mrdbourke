35869a3c5f37d1240698de7b9f0ace55f6a28a47
abhishek <bhatiaabhishek8893@gmail.com>
2021-05-01 18:56:40 +0530

    In LabelAnalyzer (src.utils) added option for test_labels to be None.

--- src/utils.py
+++ src/utils.py
@@ -32,12 +32,14 @@ def rmse(y_true, y_pred):
 
 class LabelAnalyzer:
     
-    def __init__(self, train_labels, test_labels):
+    def __init__(self, train_labels, test_labels=None):
         self.train = self._get_count_dict(train_labels)
-        self.test = self._get_count_dict(test_labels)
+        self.__has_test_data = test_labels is not None
         
-        self._make_count_df()
+        if self.__has_test_data:
+            self.test = self._get_count_dict(test_labels)
 
+        self._make_count_df()
     
     @staticmethod
     def _get_count_dict(labels):
@@ -51,6 +53,11 @@ class LabelAnalyzer:
     
     def _make_count_df(self):
         traindf = pd.DataFrame(self.train)
+
+        if not self.__has_test_data:
+            self.countdf = traindf
+            return None
+
         testdf = pd.DataFrame(self.test)
         countdf = pd.merge(traindf, testdf, how='outer', on='label', 
                            suffixes=('_train', '_test')).fillna(0).sort_values('label')
@@ -62,9 +69,19 @@ class LabelAnalyzer:
         
     def plot(self):
         ax = self.countdf.plot(x='label', kind='bar', stacked=True, figsize=(12, 4))
-        ax.legend(labels=['train', 'test'], bbox_to_anchor=(1.01, 0.6))
-        ax.set_title('Train vs Test label distribution', fontdict=dict(weight='bold', size=15))
+
+        if self.__has_test_data:
+            title = 'Train vs Test label distribution'
+            legend_labels = ['train', 'test']
+        else:
+            title = 'Train label distribution'
+            legend_labels = ['train']
+
+        ax.set_title(title, fontdict=dict(weight='bold', size=15))
+        ax.legend(labels=legend_labels, bbox_to_anchor=(1.01, 0.6))
         return ax
+
+
     
             
         
