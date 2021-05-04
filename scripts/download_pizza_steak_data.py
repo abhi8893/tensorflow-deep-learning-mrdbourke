7b6459de3af51940bd1d29468912da1a9a2a00fc
abhishek <bhatiaabhishek8893@gmail.com>
2021-05-04 17:46:16 +0530

    relates #27; Added pizza-steak data download script

---
+++ scripts/download_pizza_steak_data.py
@@ -0,0 +1,18 @@
+import wget
+import zipfile
+import os
+import shutil
+import sys
+
+DATA_URL = r'https://storage.googleapis.com/ztm_tf_course/food_vision/pizza_steak.zip'
+OUT_FILE = r'data/pizza_steak.zip'
+
+
+out_file = wget.download(DATA_URL, out=OUT_FILE)
+
+with zipfile.ZipFile(out_file, 'r') as zipref:
+    zipref.extractall('data/')
+
+os.remove(out_file)
+shutil.rmtree('data/__MACOSX')
+
