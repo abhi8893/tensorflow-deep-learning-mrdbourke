a38acee538d92a7eb29ee394d98678fd44ab3477
abhishek <bhatiaabhishek8893@gmail.com>
2021-05-31 23:18:35 +0530

    Added import for create_tensorboard_callback to __init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,6 +1,6 @@
 from .utils import (describe_tensor, get_dataframe_cols, tensor_variance, 
-                    check_tfmodel_weights_equality)
+                    check_tfmodel_weights_equality, create_tensorboard_callback)
 from ._labelanalyzer import LabelAnalyzer
 
 __all__ = ["describe_tensor", "get_dataframe_col", "tensor_variance",
-            "check_tfmodel_weights_equality", "LabelAnalyzer"]
+            "check_tfmodel_weights_equality", "LabelAnalyzer", "create_tensorboard_callback"]
