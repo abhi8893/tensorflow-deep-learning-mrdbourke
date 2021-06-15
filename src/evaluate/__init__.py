8c604b382f2c0df423d8464ad9a6e3d9f5de1810
abhi8893 <35069768+abhi8893@users.noreply.github.com>
2021-06-15 11:39:25 +0530

    fixed wrong import for ClassificationPerformanceComparer

--- src/evaluate/__init__.py
+++ src/evaluate/__init__.py
@@ -1,5 +1,5 @@
 from .keras_metrics import KerasMetrics
 from .numpy_metrics import rmse
-from .classifcation_performance_comparer import ClassificationPerformanceComparer
+from .classification_performance_comparer import ClassificationPerformanceComparer
 
 __all__ = ('KerasMetrics', 'rmse', 'ClassificationPerformanceComparer')
